419f5ec5a6b8cd94bfd9bc6e8c450851bda59479
Marcelo Carlini <marcelocarlini@hotmail.com>
2023-10-07 17:19:20 -0300

    Conclusão do Card e criação do Data.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import Navbar from "./Navbar"
 import SubClasses from "./SubClasses"
 import Card from "./Card"
+import { data } from './data.js'
 
 const Home = () => {
+    const displayCards = data ?
+        data?.map((d, key) => (
+            <Card item={d} key={key} />
+        )) :
+        <div>..processando</div>
     return (
         <>
             <header>
@@ -12,7 +18,11 @@ const Home = () => {
             </header>
             <body>
                 <div className="container_fluid">
-                    <Card />
+                    <div className="row">
+
+                        {displayCards}
+
+                    </div>
                 </div>
 
             </body>
