4fc1a84fbb6d8eb42f97e472139a7743c7287415
Marcelo Carlini <marcelocarlini@hotmail.com>
2023-10-19 23:45:04 -0300

    teste

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -7,7 +7,7 @@ import OpenAI from "openai";
 
 const { Configuration, OpenAIApi } = require('openai')
 const Rooms = () => {
-    const [tabelVal, setTavelVal] = useState(null)
+    const [tableVal, setTableVal] = useState(null)
     const [tableProcessing, setTableProcessing] = useState(false)
     const location = useLocation();
     const placeInfo = location.state.info
@@ -17,11 +17,11 @@ const Rooms = () => {
         dangerouslyAllowBrowser: true,
     });
 
-    const GererateTravelPlan = async (targetPlace) => {
+    const GenerateTravelPlan = async (targetPlace) => {
         setTableProcessing(true)
         const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: "Qual capital do Brasil?",
+            model: "gpt-3.5-turbo-0301",
+            prompt: "Qual capital do Brasil?",
             temperature: 0.7,
             max_tokens: 256,
         });
@@ -29,7 +29,7 @@ const Rooms = () => {
         setTableProcessing(false)
         console.log(response.data.choices[0].text);
     }
-    GererateTravelPlan();
+    GenerateTravelPlan();
 
     const imageGallery = placeInfo?.images.slice(1, placeInfo?.images.lenght).map((i, k) => (
         <div className='col-12 col-md-6 px-1 my-1' key={k}>
@@ -63,22 +63,38 @@ const Rooms = () => {
                 </div>
                 <div className='container my-5'>
                     <div className='text-center'>
-                        <button className='btn btn-lg btn-primary w-50 text-center' onClick={()=>{GenerateTravelPlan(placeInfo.city)}}>
+                        <button className='btn btn-lg btn-primary w-50 text-center' onClick={() => { GenerateTravelPlan(placeInfo.city) }}>
                             Crie plano de viagem para {placeInfo.city} <i className='bi bi-magic'></i>
                         </button><br />
                         <small className='fst-italic'>powered by <b>OpenAI GPT-3</b></small>
                     </div>
                     <div className='mt-4 pb-5'>
                         {
-                            tableProcessing && !tabelVal ?
+                            tableProcessing && !tableVal ?
                                 <div className='d-flex justify-content-center'>
                                     <div className='spinner-border' role="status">
                                         <span className='visually-hidden'>processar...</span>
                                     </div>
                                 </div>
-                                :!tableProcessing && tabelVal?parse(tabelVal):""
+                                : !tableProcessing && tableVal ? parse(tableVal) : ""
                         }
                         <table className='myTable'>
+                            <tr>
+                                <th>Atividade</th>
+                                <th>Hora</th>
+                                <th>Comentario</th>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Dia 1
+                                </td>
+                                <td>
+                                    9 AM
+                                </td>
+                                <td>
+                                    Escreva alguma coisa!
+                                </td>
+                            </tr>
 
                         </table>
                     </div>
